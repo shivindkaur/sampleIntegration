c647ccebdee2c68b68e0880207728a320bca8056
shivindkaur <39499040+shivindkaur@users.noreply.github.com>
2020-05-11 16:56:30 +0530

    testing maven

--- sampletesting/src/test/java/seleniumsampleproject/salesforce/CaseCreationValidationTest.java
+++ sampletesting/src/test/java/seleniumsampleproject/salesforce/CaseCreationValidationTest.java
@@ -35,7 +35,7 @@ public class CaseCreationValidationTest {
 		element_password.sendKeys(password);
 		login_button = cd.findElement(By.id("Login"));
 		login_button.click();
-		System.out.println("stopping...");
+		System.out.println("stoppin123g...");
 	}
 
 	
